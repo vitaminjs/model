f681d75dd71beda77058e969c5c31d92bdb15a54
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-07-26 14:20:21 +0200

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,1 +1,70 @@
 
+import EventEmitter from 'events'
+import Promise from 'bluebird'
+
+/**
+ * Model Event Emitter Class
+ */
+class Events extends EventeEmitter {
+  
+  /**
+   * Add a handler for the given event
+   * 
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
+   */
+  on(event, fn) {
+    event.split(' ').forEach(name => {
+      if ( name ) this.super.on(name, fn)
+    })
+    
+    return this
+  }
+  
+  /**
+   * Add a one time handler for the given event
+   * 
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
+   */
+  once(event, fn) {
+    event.split(' ').forEach(name => {
+      if ( name ) this.super.once(name, fn)
+    })
+    
+    return this
+  }
+  
+  /**
+   * Trigger an event with parameters
+   * 
+   * @param {String} event
+   * @param {Array} args
+   * @return promise
+   */
+  emit(event, ...args) {
+    return Promise.reduce(this.listeners(event), fn => {
+      return fn(...args)
+    })
+  }
+  
+  /**
+   * Remove an event handler
+   * 
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
+   */
+  off(event, fn) {
+    if ( fn ) this.removeListener(event, fn)
+    else this.removeAllListeners(event)
+    
+    return this
+  }
+  
+}
+
+// exports
+export default Events
